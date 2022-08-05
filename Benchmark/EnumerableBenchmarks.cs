4e7fbc35c95f3936c1ff54b9140e7472ae4dbc59
Jason Lautzenheiser <jlautzenheiser@new-innov.com>
2022-08-05 06:55:52 -0400

    added enumerable and ranges benchmarks

---
+++ Benchmark/EnumerableBenchmarks.cs
@@ -0,0 +1,66 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace Benchmark;
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net60)]
+public class EnumerableBenchmarks
+{
+
+
+  [Benchmark]
+  public void PassAPredicateNullAssigning()
+  {
+    IEnumerable<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    list.IsNullOrEmpty1<int>(p=>p == 2);
+  }
+
+  [Benchmark]
+  public void PassAPredicateCheckForNull()
+  {
+    IEnumerable<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    list.IsNullOrEmpty2<int>(p=>p == 2);
+  }
+
+  
+  [Benchmark]
+  public void AssignNullPredicateToTrue()
+  {
+    IEnumerable<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    list.IsNullOrEmpty1<int>();
+  }
+
+  [Benchmark]
+  public void IgnoreNullPredicate()
+  {
+    IEnumerable<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    list.IsNullOrEmpty2<int>();
+  }
+
+}
+
+public static class EnumerableExtensions
+{
+  public static bool IsNullOrEmpty1<ItemType>(this IEnumerable<ItemType>? enumerable,
+    Func<ItemType, bool>? predicate = null)
+  {
+    predicate ??= (i) => true;
+    return enumerable == null || !enumerable.Any(predicate);
+
+    // if (enumerable == null) return true;
+    // return predicate == null
+    //   ? !enumerable.Any()
+    //   : !enumerable.Any(predicate);
+  }
+
+  public static bool IsNullOrEmpty2<ItemType>(this IEnumerable<ItemType>? enumerable,
+    Func<ItemType, bool>? predicate = null)
+  {
+    if (enumerable == null) return true;
+    return predicate == null
+      ? !enumerable.Any()
+      : !enumerable.Any(predicate);
+  }
+} 
+  
---
+++ Benchmark/RangeBenchMarks.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using NI.Tools.Ranges;
+#if NET6_0_OR_GREATER
+#endif
+
+namespace Benchmark;
+
+[MemoryDiagnoser]
+[SimpleJob(runtimeMoniker: RuntimeMoniker.Net50)]
+[SimpleJob(runtimeMoniker: RuntimeMoniker.Net462)]
+public class RangeBenchMarks
+{
+#if NET6_0_OR_GREATER
+  [Benchmark]
+  public bool CheckListWithAny()
+  {
+    int count = 10;
+    var xx = (0..count).ToEnumerable();
+    return xx.Any();
+  }    
+#endif    
+
+#if !NET6_0_OR_GREATER
+  [Benchmark]
+  public bool CheckListWithAny()
+  {
+    int count = 10;
+    var xx = (0..count).ToEnumerable();
+    return xx.Any();
+  }
+#endif
+}
